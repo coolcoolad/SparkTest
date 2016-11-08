0c72fec3496b3bc37c6ad975c4806c545aea57a4
coolcoolad <441629632@qq.com>
2016-11-08 12:20:02 +0800

    new file:   SparkTest.scala

---
+++ SparkTest.scala
@@ -0,0 +1,9 @@
+﻿import org.apache.spark.SparkCOntext
+import org.apache.spark.SparkCOntext
+import org.apache.spark.SparkConf
+
+object SparkTest{
+  def main(args: Array[String]){
+    println("hello world")
+  }
+}
